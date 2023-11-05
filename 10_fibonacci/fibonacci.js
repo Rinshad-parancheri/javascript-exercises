7fc8471894177700a6fdde7defb8ad273ddfabc7
rinshadpdev <rinshadp014@gmail.com>
2023-11-05 17:38:36 +0530

    task complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  let sum = [0, 1]
+  if (n == 1 || n == 2){
+    return 1;
+  }else if (n < 0){
+    return 'OOPS'
+}
+  for (let i = 2; i <= n; i++){
 
+      sum[i] = sum[i - 1] + sum[i -2];
+ }
+ return sum[n];
+    
 };
 
 // Do not edit below this line

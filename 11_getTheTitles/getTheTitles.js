700f449ea5c0df681fd453e143bd2237410fef35
rinshadpdev <rinshadp014@gmail.com>
2023-11-06 07:17:46 +0530

    complete the task

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+  let bookTitiles = []
+   console.log(arr[0].title);
+  for (let i = 0; i < arr.length; i++){
+     bookTitiles[i] = arr[i].title;
 
+   }
+   return bookTitiles;
 };
 
 // Do not edit below this line
